78b46ee4a5745831ab26c26f7ca80580486e920f
Acask8rawK <acapasyask8@gmail.com>
2025-11-29 10:39:19 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_51422281/Pert4_51422281.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422281;
+
+/**
+ *
+ * @author Pasya Shafaa Aaqila
+ */
+public class Pert4_51422281 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
